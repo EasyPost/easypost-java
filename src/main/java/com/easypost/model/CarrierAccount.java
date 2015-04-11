36c8b278bc34f01086afc9078a9f086771875061
Jake Epstein <jake@easypost.com>
2015-04-10 17:42:27 -0700

    added ability to interact with carrier_account api endpoints

--- src/main/java/com/easypost/model/CarrierAccount.java
+++ src/main/java/com/easypost/model/CarrierAccount.java
@@ -1,11 +1,18 @@
 package com.easypost.model;
 
+import com.easypost.exception.EasyPostException;
 import com.easypost.net.EasyPostResource;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
 public class CarrierAccount extends EasyPostResource {
     public String id;
     String readable;
     String description;
+		String reference;
+		Map<String, Object> credentials;
 
     public String getId() {
         return id;
@@ -19,4 +26,70 @@ public class CarrierAccount extends EasyPostResource {
 
     public String getDescription() { return description; }
     public void setDescription(String description) { this.description = description; }
-}
+
+		public String getReference() {
+		return reference;
+	}
+		public void setReference(String reference) {
+		this.reference = reference;
+	}
+
+		public Map<String, Object> getCredentials() {
+		return credentials;
+	}
+		public void setCredentials(Map<String, Object> credentials) {
+		this.credentials = credentials;
+	}
+
+		// update
+		public CarrierAccount update(Map<String, Object> params) throws EasyPostException {
+			return this.update(params, null);
+		}
+		public CarrierAccount update(Map<String, Object> params, String apiKey) throws EasyPostException {
+			Map<String, Object> wrappedParams = new HashMap<String, Object>();
+			wrappedParams.put("carrier_account", params);
+
+			CarrierAccount response = request(RequestMethod.PUT, instanceURL(CarrierAccount.class, this.getId()), wrappedParams, CarrierAccount.class, apiKey);
+
+			this.merge(this, response);
+			return this;
+		}
+
+		// delete
+		public void delete() throws EasyPostException {
+			this.delete(null);
+		}
+		public void delete(String apiKey) throws EasyPostException {
+			request(RequestMethod.DELETE, instanceURL(CarrierAccount.class, this.getId()), null, CarrierAccount.class, apiKey);
+		}
+
+		// create
+		public static CarrierAccount create(Map<String, Object> params) throws EasyPostException {
+			return create(params, null);
+		}
+		public static CarrierAccount create(Map<String, Object> params, String apiKey) throws EasyPostException {
+			Map<String, Object> wrappedParams = new HashMap<String, Object>();
+			wrappedParams.put("carrier_account", params);
+
+			return request(RequestMethod.POST, classURL(CarrierAccount.class), wrappedParams, CarrierAccount.class, apiKey);
+		}
+
+		// retrieve
+		public static CarrierAccount retrieve(String id) throws EasyPostException {
+			return retrieve(id, null);
+		}
+		public static CarrierAccount retrieve(String id, String apiKey) throws EasyPostException {
+			return request(RequestMethod.GET, instanceURL(CarrierAccount.class, id), null, CarrierAccount.class, apiKey);
+		}
+
+		// all
+		public static List<CarrierAccount> all(Map<String, Object> params) throws EasyPostException {
+			return all(params, null);
+		}
+		public static List<CarrierAccount> all(Map<String, Object> params, String apiKey) throws EasyPostException {
+			return request(RequestMethod.GET, classURL(CarrierAccount.class), params, List.class, apiKey);
+		}
+
+
+
+	}
