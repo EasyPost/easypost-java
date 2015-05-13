8dd88d55d5d64b21ebe56cca98145917f1fcedb8
Andrew Tribone <atribone@gmail.com>
2015-05-13 12:21:22 -0700

    Make API_BASE configurable

--- src/main/java/com/easypost/EasyPost.java
+++ src/main/java/com/easypost/EasyPost.java
@@ -2,7 +2,7 @@ package com.easypost;
 
 public abstract class EasyPost
 {
-	public static final String API_BASE = "https://api.easypost.com/v2";
+	public static String API_BASE = "https://api.easypost.com/v2";
 	public static final String VERSION = "2.1.0";
 	public static String apiKey;
 	public static String apiVersion;
