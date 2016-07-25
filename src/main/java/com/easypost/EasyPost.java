ff1cff35f42c553d327db70e02f1b7fcc7c149a6
Jake Epstein <jake@easypost.com>
2016-07-25 15:06:49 -0700

    bump version to 2.2.7

--- src/main/java/com/easypost/EasyPost.java
+++ src/main/java/com/easypost/EasyPost.java
@@ -3,7 +3,7 @@ package com.easypost;
 public abstract class EasyPost
 {
   public static String API_BASE = "https://api.easypost.com/v2";
-  public static final String VERSION = "2.2.6";
+  public static final String VERSION = "2.2.7";
   public static String apiKey;
   public static String apiVersion;
   public static int readTimeout;
