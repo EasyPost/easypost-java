b539d9651689ef55fa8ccd108298ec6ab3d5ed9b
Jake Epstein <jake@easypost.com>
2016-12-14 16:09:24 -0800

    bumped version

--- src/main/java/com/easypost/EasyPost.java
+++ src/main/java/com/easypost/EasyPost.java
@@ -3,7 +3,7 @@ package com.easypost;
 public abstract class EasyPost
 {
   public static String API_BASE = "https://api.easypost.com/v2";
-  public static final String VERSION = "3.0.5";
+  public static final String VERSION = "3.1.0";
   public static String apiKey;
   public static String apiVersion;
   public static int readTimeout;
