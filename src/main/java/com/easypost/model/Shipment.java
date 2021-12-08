421149d95896c15dc85c8a9b6d62a926f81b4685
jchen293 <jchen293@buffalo.edu>
2021-12-08 17:41:33 -0500

    Update src/main/java/com/easypost/model/Shipment.java
    Co-authored-by: Justin Hammond <39606064+Justintime50@users.noreply.github.com>

--- src/main/java/com/easypost/model/Shipment.java
+++ src/main/java/com/easypost/model/Shipment.java
@@ -226,7 +226,7 @@ public class Shipment extends EasyPostResource {
 		this.forms = forms;
 	}
 
-	public List<TaxIdentifier> getTaxIdentifier(){
+	public List<TaxIdentifier> getTaxIdentifiers(){
 		return taxIdentifiers;
 	}
 	public void setTaxIdentifiers(List<TaxIdentifier> taxIdentifiers){
