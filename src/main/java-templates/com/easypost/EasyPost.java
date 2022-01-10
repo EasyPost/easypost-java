15723e3e87db0e3733d1fcdc0798d871607e7e85
Nate Harris <nharris@easypost.com>
2022-01-10 13:10:01 -0700

    - Added suppressions to EasyPost.java

--- src/main/java-templates/com/easypost/EasyPost.java
+++ src/main/java-templates/com/easypost/EasyPost.java
@@ -1,9 +1,16 @@
 package com.easypost;
 
 public abstract class EasyPost {
+    @SuppressWarnings("checkstyle:variablename")
+    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static String API_BASE = "https://api.easypost.com/v2";
+    @SuppressWarnings("checkstyle:variablename")
+    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static final String VERSION = "${project.version}";
+    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static String apiKey;
+    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static String apiVersion;
+    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static int readTimeout;
 }
