ffc557a5b6b53e6ada8a6d702361da50b09ef1be
Nate Harris <nharris@easypost.com>
2022-01-10 13:19:32 -0700

    - Rollback EasyPost.java suppression annotations for now, will tackle in separate PR

--- src/main/java-templates/com/easypost/EasyPost.java
+++ src/main/java-templates/com/easypost/EasyPost.java
@@ -1,16 +1,9 @@
 package com.easypost;
 
 public abstract class EasyPost {
-    @SuppressWarnings("checkstyle:variablename")
-    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static String API_BASE = "https://api.easypost.com/v2";
-    @SuppressWarnings("checkstyle:variablename")
-    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static final String VERSION = "${project.version}";
-    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static String apiKey;
-    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static String apiVersion;
-    @SuppressWarnings("checkstyle:visibilitymodifier")
     public static int readTimeout;
 }
