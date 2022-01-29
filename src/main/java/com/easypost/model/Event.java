975ddc2015130d43f3d90029ceb50d8a04bd473a
jchen293 <jchen@easypost.com>
2022-01-28 19:24:55 -0500

    remove copyright header.

--- src/main/java/com/easypost/model/Event.java
+++ src/main/java/com/easypost/model/Event.java
@@ -1,10 +1,3 @@
-/**
- * Event.java
- * This file is a part of EasyPost API SDK.
- * (c) 2022 EasyPost
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 package com.easypost.model;
 
 import com.easypost.exception.EasyPostException;
