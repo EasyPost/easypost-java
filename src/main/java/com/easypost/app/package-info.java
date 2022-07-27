9ee7e5236711f54bc3bcd2a30a16ebf0b2737c4c
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-07-27 16:09:24 -0600

    chore: removes examples directory

--- src/main/java/com/easypost/app/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Executable examples of the EasyPost API Java SDK.
- *
- * @author EasyPost developers
- * @version 1.0
- * @see <a href="https://www.easypost.com/docs">EasyPost API documentation</a>
- * @since 1.0
- */
-package com.easypost.app;
