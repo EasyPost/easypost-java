b7924f7692ce2874c585c3897c6c87e6a151a1ba
jchen293 <jchen@easypost.com>
2022-10-06 12:39:28 -0400

    remove a unused import

--- src/main/java/com/easypost/net/EasyPostResource.java
+++ src/main/java/com/easypost/net/EasyPostResource.java
@@ -44,7 +44,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-// import java.util.Random;
 import java.util.Scanner;
 
 public abstract class EasyPostResource {
