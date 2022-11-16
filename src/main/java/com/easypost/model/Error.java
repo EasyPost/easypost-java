945fc446d3388a9421aaa5c5fc03d317d7a01dac
jchen293 <jchen@easypost.com>
2022-11-16 15:09:15 -0500

    make setters as default access

--- src/main/java/com/easypost/model/Error.java
+++ src/main/java/com/easypost/model/Error.java
@@ -2,13 +2,30 @@ package com.easypost.model;
 
 import java.util.List;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
 public final class Error {
-    private @Setter String message;
-    private @Setter String code;
+    private String message;
+    private String code;
     private List<Error> errors;
     private String suggestion;
     private String field;
+
+    /**
+     * Set the message of this error object.
+     *
+     * @param message The error message.
+     */
+    void setMessage(final String message) {
+        this.message = message;
+    }
+
+    /**
+     * Set the code of this error object.
+     *
+     * @param code The error code.
+     */
+    void setCode(final String code) {
+        this.code = code;
+    }
 }
