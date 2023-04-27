543d7a7a2ea43550555bbf21d4900d7cb290d6a6
Junjie(Jack) Chen <junjiechen0620@gmail.com>
2023-04-27 15:44:32 -0400

    add retrieveEstimatedDeliveryDate function in Shipment class (#262)

---
+++ src/main/java/com/easypost/model/EstimatedDeliveryDateResponse.java
@@ -0,0 +1,9 @@
+package com.easypost.model;
+
+import java.util.List;
+import lombok.Getter;
+
+@Getter
+public class EstimatedDeliveryDateResponse {
+    private List<EstimatedDeliveryDate> rates;
+}
