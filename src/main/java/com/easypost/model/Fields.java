1c374d850135bf87cb5826768d4954e10f6c860b
Nate Harris <nwithan8@users.noreply.github.com>
2024-04-12 12:39:36 -0600

    - Fix invalid schema for carrier account field/fields

--- src/main/java/com/easypost/model/Fields.java
+++ src/main/java/com/easypost/model/Fields.java
@@ -2,6 +2,12 @@ package com.easypost.model;
 
 import lombok.Getter;
 
+import java.util.Map;
+
 @Getter
 public class Fields extends EasyPostResource {
+    private Map<String, Field> credentials;
+    private Map<String, Field> testCredentials;
+    private boolean autoLink;
+    private boolean customWorkflow;
 }
