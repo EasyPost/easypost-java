f846cd1152bba1fee520783b4cea7dac2142adf9
jchen293 <jchen@easypost.com>
2024-07-23 17:46:06 -0400

    address comment

--- src/main/java/com/easypost/model/ClaimCollection.java
+++ src/main/java/com/easypost/model/ClaimCollection.java
@@ -5,10 +5,15 @@ import java.util.Map;
 
 import com.easypost.exception.General.EndOfPaginationError;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public final class ClaimCollection extends PaginatedCollection<Claim> {
     private List<Claim> claims;
+
+    @Setter
+    private String type;
+    private String status;
 
     @Override
     protected Map<String, Object> buildNextPageParameters(List<Claim> claims, Integer pageSize)
@@ -17,6 +22,8 @@ public final class ClaimCollection extends PaginatedCollection<Claim> {
 
         Map<String, Object> parameters = new java.util.HashMap<>();
         parameters.put("before_id", lastId);
+        parameters.put("type", type);
+        parameters.put("status", status);
 
         if (pageSize != null) {
             parameters.put("page_size", pageSize);
