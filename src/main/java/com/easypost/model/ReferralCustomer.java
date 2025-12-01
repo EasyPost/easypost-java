2c99aa0651df15cce9926b084c6cc621a462e479
Josh Lane <joshlane@easypost.com>
2025-12-01 15:26:36 -0800

    fix: remove duplicate apiKeys field from ReferralCustomer
    ReferralCustomer extends BaseUser, which now has the apiKeys field.
    Having apiKeys in both classes causes a Gson duplicate field error
    during JSON deserialization.

--- src/main/java/com/easypost/model/ReferralCustomer.java
+++ src/main/java/com/easypost/model/ReferralCustomer.java
@@ -1,10 +1,8 @@
 package com.easypost.model;
 
-import java.util.List;
-
 import lombok.Getter;
 
 @Getter
 public class ReferralCustomer extends BaseUser {
-    private List<ApiKey> apiKeys;
+    // apiKeys is inherited from BaseUser
 }
